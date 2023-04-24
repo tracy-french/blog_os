a75eed20ec0b4b4afdc07d1c90a12559fbec4bcd
Tracy French <67283114+tracy-french@users.noreply.github.com>
2023-04-23 19:12:51 -0600

    bare metal kernel

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,25 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
     loop {}
 }
 
-// this function is called on panic
+/// this function is called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
